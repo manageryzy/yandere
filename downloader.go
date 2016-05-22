9691b3e9b5dcc631855e1611a3321fe8c9cc7090
manageryzy <manageryzy@gmail.com>
2016-05-23 01:26:38 +0900

    multi thread Signed-off-by: manageryzy <manageryzy@gmail.com>

--- downloader.go
+++ downloader.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -12,17 +11,17 @@ import (
 )
 
 func HTTPDownload(uri string) ([]byte, error) {
-	fmt.Printf("HTTPDownload From: %s.\n", uri)
+	//fmt.Printf("HTTPDownload From: %s.\n", uri)
 	res, err := http.Get(uri)
 	if err != nil {
-		log.Fatal(err)
+		println(err)
 	}
 	defer res.Body.Close()
 	d, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		println(err)
 	}
-	fmt.Printf("ReadFile: Size of download: %d\n", len(d))
+	//fmt.Printf("ReadFile: Size of download: %d\n", len(d))
 	return d, err
 }
 
@@ -30,51 +29,77 @@ func WriteFile(dst string, d []byte) error {
 	fmt.Printf("WriteFile: Size of download: %d\n", len(d))
 	err := ioutil.WriteFile(dst, d, 0444)
 	if err != nil {
-		log.Fatal(err)
+		println(err)
 	}
 	return err
 }
 
 func DownloadToFile(uri string, dst string) {
-	fmt.Printf("DownloadToFile From: %s.\n", uri)
+	//fmt.Printf("DownloadToFile From: %s.\n", uri)
 	if d, err := HTTPDownload(uri); err == nil {
-		fmt.Printf("downloaded %s.\n", uri)
+		//fmt.Printf("downloaded %s.\n", uri)
 		if WriteFile(dst, d) == nil {
-			fmt.Printf("saved %s as %s\n", uri, dst)
+			//fmt.Printf("saved %s as %s\n", uri, dst)
 		}
 	}
 }
 
-var startID,endID int;
+var startID,endID,workers int;
+var jobs = make([]int,0)
+var c chan int;
 
 func init() {
 	flag.IntVar(&startID,"start",1,"start number")
 	flag.IntVar(&endID,"end",355850,"end number")
+	flag.IntVar(&workers,"workers",4,"download thread number")
+
+	flag.Parse()
 }
 
 func main() {
 	os.MkdirAll("image",666);
+
+
 	for i:=startID;i<=endID;i++{
-		downloadImage(i)
+		jobs = append(jobs,i)
 	}
+
+	for i:=0;i<workers;i++{
+		go downloadWorker()
+	}
+
+	for i := 0; i <workers; i++ {
+		<-c
+	}
+}
+
+func downloadWorker(){
+	defer func() {
+		c<-0;
+		println("worker exit")
+	}()
+
+	x := 0
+	x, jobs = jobs[len(jobs)-1], jobs[:len(jobs)-1]
+
+	downloadImage(x)
 }
 
 
 func downloadImage(id int){
+	println("querying " + strconv.Itoa(id))
 	doc, err := goquery.NewDocument("https://yande.re/post/show/"+strconv.Itoa(id))
 	if err != nil {
-		log.Fatal(err)
+		println(err)
 	}
 
 	doc.Find("#highres").Each(func(i int, s *goquery.Selection) {
 		href,exist := s.Attr("href")
 		if exist{
-			println("downloading:"+strconv.Itoa(id)+"@"+href)
+			println("downloading:"+strconv.Itoa(id)+"\t@"+href)
 			DownloadToFile(href,"./image/"+strconv.Itoa(id)+".jpg")
 		}else {
 			println("fail to download "+strconv.Itoa(id))
 		}
 	})
-
-
 }
